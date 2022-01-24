e5d6fc7a8461dec29cc7e9be76559c714e6d554f
KIRUYXAN <279901@gmail.com>
2022-01-24 21:36:46 +0300

    .

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,24 @@
 ﻿/*Изменить программу вывода таблицы функции так, чтобы можно было передавать функции типа double
 (double, double). Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).*/
+
+public delegate double Fun(double x, double a);
+
+class Program
+{
+    public static void Table(Fun F, double x, double a)
+    {
+        Console.WriteLine(" ---- X ---------- A ---------- Y -----");
+        Console.WriteLine($"| {x,10:0.000} | {a,10:0.000} | {F(x,a),10:0.000} |");
+        Console.WriteLine(" --------------------------------------");
+    }
+    public static double MyFunc1(double x, double a) => a * (x * x);
+    public static double MyFunc2(double x, double a) => a * Math.Sin(x);
+
+    static void Main()
+    {
+        Console.WriteLine("Таблица функции MyFunc1:");
+        Table(MyFunc1, 2, 5);
+        Console.WriteLine("Таблица функции MyFunc2:");
+        Table(MyFunc2, 2, 5);
+    }
+}
